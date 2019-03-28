867239ace07584627991d0b62540105ae42299eb
BradHick <bradleyhrm@gmail.com>
2019-03-27 23:47:35 -0300

    feat: create a generate helper

---
+++ cypress/utils/generate.js
@@ -0,0 +1,19 @@
+const faker = require('faker');
+
+module.exports = {
+
+  generateUser: () => ({
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  }),
+
+  generateRecipe: () => ({
+    title: faker.lorem.sentence(),
+    description: faker.lorem.sentence(4),
+    timeToCook: faker.random.number(),
+    ingredients: [faker.lorem.sentence(), faker.lorem.sentence()],
+    procedure: [faker.lorem.sentence(), faker.lorem.sentence()]
+  })
+
+};
